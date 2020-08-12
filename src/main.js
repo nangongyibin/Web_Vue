0ead102de15ed26a6ac173dc2c926b07ca95c900
南宫燚滨 <nangongyibin@gmail.com>
2020-08-12 08:51:37 +0800

    更新4个文件。

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import { DataPicker} from 'ant-design-vue'
 
 Vue.config.productionTip = false
+// Vue.use(DataPicker)
 
 /* eslint-disable no-new */
 new Vue({
